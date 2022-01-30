4769e217751a1ff33859714c4de4e7f0154210dd
docteurjs <mahmoudbarrysn@gmail.com>
2022-01-30 15:08:09 +0000

    Rank component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from "./components/Logo/Logo";
 import Navigation from './components/navigation/Navigation';
+import Rank from './components/Rank/Rank';
+
+
 function App() {
   return (
     <div className="App">
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
+        
     </div>
   );
 }
